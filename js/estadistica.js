329d4e4845389f5b1c33cd39473c8e2f037d7682
Jurek Hernandez <jurek.hernandez@gmail.com>
2018-03-12 10:23:38 -0300

    probando 12-03

--- js/estadistica.js
+++ js/estadistica.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
     estadistica();
+    setTimeout(
+        function(){
+            estadistica();
+        }, 5000);
 })
 
 function estadistica(){
     $(".producto").remove();
     $.ajax({
         type: "post",
-        url: "http://localhost/prueba_wb/public/estadistica",
-        cache: false,
-     //  url:"http://35.202.228.28/prueba_wb/public/estadistica",
+      //  url: "http://localhost/prueba_wb/public/estadistica",
+          url:"http://35.202.228.28/prueba_wb/public/estadistica",
+          cache: false,
         dataType: "json",
     }).done(function(data){
         console.log(data);
@@ -26,4 +30,19 @@ function estadistica(){
         console.log("algo salio mal:");
         console.log(error);
     });
+}
+
+function estadisticaget(){
+$.get('http://35.202.228.28/prueba_wb/public/es', function( data ) {
+    for(var i =0 ; i< data.length; i++){
+        var elementos = "";
+        elementos+="<tr class'producto'>";
+        elementos+="<th scope='row'>"+data[i].id_producto+"</th>"; 
+        elementos+="<td>"+data[i].titulo+"</td>";
+        elementos+="<td>"+data[i].cantidad+"</td>";
+        elementos+="<td>"+data[i].palabras+"</td>";
+        elementos+="</tr>";
+        $("#filas").append(elementos);
+    }
+}, "json" );
 }
