62c4ade08d826ae9e7d62e593dd182a5ce75d35e
Jurek Hernandez <jurek.hernandez@gmail.com>
2018-03-11 22:16:14 -0300

    probando

--- js/estadistica.js
+++ js/estadistica.js
@@ -6,25 +6,24 @@ function estadistica(){
     $(".producto").remove();
     $.ajax({
         type: "post",
-       url: "http://localhost/prueba_wb/public/estadistica",
+        url: "http://localhost/prueba_wb/public/estadistica",
+        cache: false,
      //  url:"http://35.202.228.28/prueba_wb/public/estadistica",
         dataType: "json",
-        success: function(data){
-            console.log(data);
-            for(var i =0 ; i< data.length; i++){
-                var elementos = "";
-                elementos+="<tr class'producto'>";
-                elementos+="<th scope='row'>"+data[i].id_producto+"</th>"; 
-                elementos+="<td>"+data[i].titulo+"</td>";
-                elementos+="<td>"+data[i].cantidad+"</td>";
-                elementos+="<td>"+data[i].palabras+"</td>";
-                elementos+="</tr>";
-                $("#filas").append(elementos);
-            }
-        },
-        error: function(errMsg) {
-            console.log("algo salio mal:");
-            console.log(errMsg);
+    }).done(function(data){
+        console.log(data);
+        for(var i =0 ; i< data.length; i++){
+            var elementos = "";
+            elementos+="<tr class'producto'>";
+            elementos+="<th scope='row'>"+data[i].id_producto+"</th>"; 
+            elementos+="<td>"+data[i].titulo+"</td>";
+            elementos+="<td>"+data[i].cantidad+"</td>";
+            elementos+="<td>"+data[i].palabras+"</td>";
+            elementos+="</tr>";
+            $("#filas").append(elementos);
         }
+    }).fail(function(error){
+        console.log("algo salio mal:");
+        console.log(error);
     });
 }
